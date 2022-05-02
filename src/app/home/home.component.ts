afc2f29cab58c023fa928c75c8930c5ce83e2611
unciax <unciaq@gmail.com>
2022-05-02 13:31:07 +0800

    feat: add ways of contest (without schedule and prize)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  title = 'hackathon-site';
+export class HomeComponent implements OnInit {
+
+  isOpenSignUp: boolean = false;
+
+  @Output()
+  callToActionText: String = "";
+
+  ngOnInit() {
+    this.isOpenSignUp = true;
+    this.callToActionText = this.isOpenSignUp ? "立即報名" : "即將開放報名";
+  }
 }
