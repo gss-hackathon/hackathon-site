7b8e93c804e8dfe5493f21e45edbaf5d2480b78c
unciax <unciaq@gmail.com>
2022-05-02 11:34:17 +0800

    feat: add homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/2022/home', pathMatch: 'full' },
+  {
+      path: '2022',
+      component: AppComponent,
+      children: [
+          { path: '', redirectTo: 'home', pathMatch: 'full' },
+          { path: 'home', component: HomeComponent },
+          { path: '**', redirectTo: '/2022/home', pathMatch: 'full' },
+      ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
