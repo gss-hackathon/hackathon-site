47eaf924b24256d94d7fdd18b8feab805ade9ad5
unciax <unciaq@gmail.com>
2022-05-07 12:45:52 +0800

    feat: change color to light & add form link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/2022/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/2022/school', pathMatch: 'full' },
   {
       path: '2022',
       children: [
-          { path: '', redirectTo: 'home', pathMatch: 'full' },
-          { path: 'home', component: HomeComponent },
-          { path: '**', redirectTo: '/2022/home', pathMatch: 'full' },
+          { path: '', redirectTo: 'school', pathMatch: 'full' },
+          { path: 'school', component: HomeComponent },
+          { path: '**', redirectTo: '/2022/school', pathMatch: 'full' },
       ]
   }
 ];
