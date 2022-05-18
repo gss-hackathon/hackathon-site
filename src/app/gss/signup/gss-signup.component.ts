fda4ac7f1a1ac43c3106237308be7af76c9a2aaa
unciax <unciaq@gmail.com>
2022-05-18 19:17:34 +0800

    fix: cta button not enable on safari

--- src/app/gss/signup/gss-signup.component.ts
+++ src/app/gss/signup/gss-signup.component.ts
@@ -7,21 +7,14 @@ import { Component, OnInit, Output } from '@angular/core';
 })
 export class GSSSignUpComponent implements OnInit {
 
-  isOpenSignUp: boolean = false;
-
-  startDate: Date = new Date('2022-05-18 15:30:00');
-
   @Output()
   callToActionText: String = "";
 
   ngOnInit() {
-    this.isOpenSignUp = (new Date() > this.startDate) ? true : false;
-    this.callToActionText = this.isOpenSignUp ? "立即報名" : "即將開放報名";
+    this.callToActionText = "立即報名";
   }
 
   openForm() {
-    if (this.isOpenSignUp) {
-      window.open('https://gsskm.gss.com.tw/survey/filloutsurvey/e0553a58d4e4417680b3b4566bb42131', '_blank');
-    }
+    window.open('https://gsskm.gss.com.tw/survey/filloutsurvey/e0553a58d4e4417680b3b4566bb42131', '_blank');
   }
 }
--- src/app/school/signup/school-signup.component.ts
+++ src/app/school/signup/school-signup.component.ts
@@ -7,21 +7,14 @@ import { Component, OnInit, Output } from '@angular/core';
 })
 export class SchoolSignUpComponent implements OnInit {
 
-  isOpenSignUp: boolean = false;
-
-  startDate: Date = new Date('2022-05-09 15:00:00');
-
   @Output()
   callToActionText: String = "";
 
   ngOnInit() {
-    this.isOpenSignUp = (new Date() > this.startDate) ? true : false;
-    this.callToActionText = this.isOpenSignUp ? "立即報名" : "即將開放報名";
+    this.callToActionText = "立即報名";
   }
 
   openForm() {
-    if (this.isOpenSignUp) {
-      window.open('https://gsskm.gss.com.tw/survey/a/637775f0-b610-41ee-a810-db8fde2b4697', '_blank');
-    }
+    window.open('https://gsskm.gss.com.tw/survey/a/637775f0-b610-41ee-a810-db8fde2b4697', '_blank');
   }
 }
