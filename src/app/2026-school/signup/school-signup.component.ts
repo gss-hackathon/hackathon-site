1d3826d5d64dc5c814083f9e4942b641636137fd
patricy_chang <patricy.cscg@gmail.com>
2026-08-07 13:41:41 +0800

    component.ts 去重複

--- src/app/2026-school/signup/school-signup.component.ts
+++ src/app/2026-school/signup/school-signup.component.ts
@@ -1,76 +1,33 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AwardModel } from './award-model';
 
 @Component({
-    selector: 'school-signup',
-    templateUrl: './school-signup.component.html',
-    styleUrls: ['./school-signup.component.scss']
-})
-export class School2026SignUpComponent {
-
-    awards: AwardModel[] = [];
-
-    private _jsonURL = 'assets/data/awards_2025.json';
-
-    constructor(private http: HttpClient) {
-        this.getJSON().subscribe(data => {
-        this.convertToModels(data);
-        });
-    }
-
-    getJSON(): Observable<any> {
-        return this.http.get(this._jsonURL);
-    }
-
-    convertToModels(data: any) {
-        for (let index in data) {
-            let award = new AwardModel();
-            award.prizeId = data[index]['prizeId'];
-            award.prizeName = data[index]['prizeName'];
-            award.projectName = data[index]['projectName'];
-            award.schoolName = data[index]['schoolName'];
-            award.url = data[index]['url'];
-            this.awards.push(award);
-        }
-    }
-
-    gotoYT(url: string): void {
-        window.open(url);
-    }
-
-    openGssWebChat() {
-        const width = 950;
-        const height = 650;
-        window.open('https://cai.gss.com.tw/webchat/index.html?botid=baseinfo', 'HelloGSSWebChat', `width=${width},height=${height},resizable=yes,scrollbars=yes`);
-    }
-
-    getTodayMonthDay(): string {
-        const today = new Date();
-        const formattedMonth = (today.getMonth() + 1).toString().padStart(2, '0');
-        const formattedDay = today.getDate().toString().padStart(2, '0');
-        return `${formattedMonth}/${formattedDay}`;
-    }
-}
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-school-signup',
+  selector: 'school-signup',
   templateUrl: './school-signup.component.html',
   styleUrls: ['./school-signup.component.scss']
 })
-export class SchoolSignupComponent implements OnInit {
+export class School2026SignUpComponent implements OnInit {
 
-  // 定義顯示次數的變數
+  awards: AwardModel[] = [];
   hitCount: string = '載入中...';
 
+  private _jsonURL = 'assets/data/awards_2025.json';
+
+  constructor(private http: HttpClient) {}
+
   ngOnInit(): void {
+    // 初始化載入獎項資料與點擊計數器
+    this.getJSON().subscribe(data => {
+      this.convertToModels(data);
+    });
+
     this.getHitCount();
   }
 
+  // 取得頁面點擊計數
   getHitCount(): void {
-    // 使用免 key、高穩定度且支援跨域的 Counter API
     const API_URL = 'https://api.counterapi.dev/v1/gss-hackathon-site/views/up';
 
     fetch(API_URL)
@@ -84,5 +41,42 @@ export class SchoolSignupComponent implements OnInit {
         console.error('計數器載入失敗:', err);
         this.hitCount = '1';
       });
+  }
+
+  getJSON(): Observable<any> {
+    return this.http.get(this._jsonURL);
+  }
+
+  convertToModels(data: any): void {
+    for (let index in data) {
+      let award = new AwardModel();
+      award.prizeId = data[index]['prizeId'];
+      award.prizeName = data[index]['prizeName'];
+      award.projectName = data[index]['projectName'];
+      award.schoolName = data[index]['schoolName'];
+      award.url = data[index]['url'];
+      this.awards.push(award);
+    }
+  }
+
+  gotoYT(url: string): void {
+    window.open(url);
+  }
+
+  openGssWebChat(): void {
+    const width = 950;
+    const height = 650;
+    window.open(
+      'https://cai.gss.com.tw/webchat/index.html?botid=baseinfo',
+      'HelloGSSWebChat',
+      `width=${width},height=${height},resizable=yes,scrollbars=yes`
+    );
+  }
+
+  getTodayMonthDay(): string {
+    const today = new Date();
+    const formattedMonth = (today.getMonth() + 1).toString().padStart(2, '0');
+    const formattedDay = today.getDate().toString().padStart(2, '0');
+    return `${formattedMonth}/${formattedDay}`;
   }
 }
